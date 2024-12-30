881d77d9096ca10e2b6a2bd7d87c982224ba4dcb
Maximiliano Chamas <datita.info@gmail.com>
2024-12-29 22:36:10 -0300

    feat: ajustes fechas

--- config/theme.ts
+++ config/theme.ts
@@ -51,9 +51,9 @@ export const theme = {
 
   // Configuración de fechas importantes
   dates: {
-    event: '2024-11-30T10:21:30', // Fecha del evento
+    event: '2024-12-31T10:21:30', // Fecha del evento
     contentActivation: '2024-10-01T00:00:00', // Fecha de activación del contenido
-    rsvpDeadline: '2024-12-31T00:00:00', // Fecha límite para confirmar asistencia
+    rsvpDeadline: '2025-12-31T00:00:00', // Fecha límite para confirmar asistencia
     liveEnd: '2024-12-30T23:59:59', // Fecha en que desaparece el indicador LIVE
   },
 
